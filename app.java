dc66cafe5059c8f66c69063c340ece9d3ab2d456
zhangmingli <zhangmingli@tenxcloud.com>
2019-08-21 18:55:06 +0800

    #17 #18 #19

--- app.java
+++ app.java
@@ -1,1 +1,12 @@
-app.java
+public class ExceptionDemo {
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+		int a = 10;
+		// int b = 4;
+		int b = 0; // java.lang.ArithmeticException: / by zero
+		System.out.println(a / b);
+
+		System.out.println("world");
+	}
+}
